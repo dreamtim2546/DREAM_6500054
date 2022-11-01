44aede6076d93a3b4f3d027703e458edff449d11
dreamtim2546 <108563244+dreamtim2546@users.noreply.github.com>
2022-11-01 13:57:10 +0700

    hello

--- dreamProject/Assets/scripts/Controllre.cs
+++ dreamProject/Assets/scripts/Controllre.cs
@@ -7,7 +7,7 @@ public class Controllre : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("hello");
     }
 
     // Update is called once per frame
